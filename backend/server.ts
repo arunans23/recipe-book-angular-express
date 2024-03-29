de96b2714dcb0e3ddc89914caf14bbc2130c8bd7
Arunan Sugunakumar <arunans23@gmail.com>
2024-03-29 22:13:11 +0530

    Add backend with one resource

---
+++ backend/server.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import { Recipe } from './models/recipes';
+import { Ingredient } from './models/ingredient';
+
+//Define the Port
+const PORT = 5001;
+
+//Initialize Express App
+const app = express();
+app.use(cookieParser());
+
+var recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', [
+      new Ingredient("French Fries", 2),
+      new Ingredient("Pork Meat", 1)
+    ]),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])];
+
+app.get("/recipes", (req: Request, res: Response) => {
+    res.send(recipes);
+});
+
+//Start the app and listen on the PORT
+app.listen(PORT, () => { console.log(`Server Started at PORT ${ PORT }`); });
